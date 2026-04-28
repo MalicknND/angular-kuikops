7ba811fc3ac0a044f4f2640e8284677eec7fc76f
MalicknND <ndiayemalick062@gmail.com>
2026-04-28 09:54:13 +0200

    feat: ajouter le composant Produits avec routage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { Produits } from './produits/produits';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'produits', component: Produits }];
